d22a6c76baa5883a1df8f40571f5417ba414a3e6
Gražvydas Jučius <grazjuc@gmail.com>
2017-12-18 10:43:40 +0200

    Looped sound modification

--- loopedSound.ts
+++ loopedSound.ts
@@ -1,19 +1,37 @@
-let currentPitch = 0
+let maxFreq = 0
 let direction = 0
+let currentPitch = 0
+let currentSpeed = 0
 let initialPitch = 0
+input.onButtonPressed(Button.A, () => {
+    if (currentSpeed > 0) {
+        currentSpeed += -1
+    }
+})
+input.onButtonPressed(Button.B, () => {
+    if (currentSpeed < 100) {
+        currentSpeed += 1
+    }
+})
 initialPitch = 10
 currentPitch = initialPitch
 direction = 0
+maxFreq = 19000
+currentSpeed = 8
 basic.forever(() => {
-    if (currentPitch > 20000) {
+    if (currentPitch > maxFreq) {
         direction = 1
     } else if (currentPitch < initialPitch) {
         direction = 0
     }
     music.ringTone(currentPitch)
+    led.plotBarGraph(
+    currentPitch,
+    maxFreq
+    )
     if (direction == 0) {
-        currentPitch += 5
+        currentPitch += currentSpeed
     } else if (direction == 1) {
-        currentPitch += -5
+        currentPitch += -1 * currentSpeed
     }
 })
